1753995191b68e32445db8f7a84f56462c4df5c5
Daniel Huckins <dhuckins@users.noreply.github.com>
2019-06-09 19:30:35 -0400

    register events for PR

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
-import * as shell from 'shelljs'
+import { Application, Context } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  if (shell.which('black')) {
-    app.log.info('***yay! have black')
-  } else {
-    app.log.error('***WTF! dont have black')
-  }
-
-  app.on('issues.opened', async (context) => {
+  app.on('issues.opened', async (context: Context) => {
     const issueComment = context.issue({
       body: 'Thanks for opening this issue!'
     })
     await context.github.issues.createComment(issueComment)
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on('pull_request.opened', async (context: Context) => {
+    await context.github.issues.createComment(context.issue({
+      body: 'This is a created pull request'
+    }))
+  })
+
+  app.on('pull_request.ready_for_review', async (context: Context) => {
+    await context.github.issues.createComment(context.issue({
+      body: 'The pull request is ready for review'
+    }))
+  })
 }
