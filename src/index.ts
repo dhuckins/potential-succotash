b9c4ad3bef88de41f4acd2aecacb7e7b033ca458
Daniel Huckins <dhuckins@users.noreply.github.com>
2019-06-15 21:34:43 -0400

    try to get a list of files from the PR

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,21 @@ export = (app: Application) => {
     await context.github.issues.createComment(issueComment)
   })
 
-  app.on('pull_request.opened', async (context: Context) => {
-    let pullRequest = context.payload.pull_request
-    await context.github.issues.createComment(context.issue({
-      body: JSON.stringify(pullRequest, null, '  ')
-    }))
-  })
+  app.on(
+    ['pull_request.opened', 'pull_request.reopened'],
+    async (context: Context) => {
+      let pullRequest = context.payload.pull_request
+      const { owner, repo, number } = context.issue()
+      let prFiles = context.github.pullRequests.listFiles({
+        owner, repo, number
+      })
+      await context.github.issues.createComment(context.issue({
+        body: JSON.stringify(pullRequest, null, '  ')
+      }))
+      await context.github.issues.createComment(context.issue({
+        body: JSON.stringify(prFiles)
+      }))
+    })
 
   app.on('pull_request.ready_for_review', async (context: Context) => {
     await context.github.issues.createComment(context.issue({
